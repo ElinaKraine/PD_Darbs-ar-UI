c323482ddb2eb2faa9a9364136bdd5dba87f0794
ElinaKraine <elinaliepkr@gmail.com>
2023-05-03 21:36:35 +0300

    Saka veidot otro varoni

---
+++ Assets/Skripti/AinasParsledzeja.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AinasParsledzeja : MonoBehaviour {
+	public void uzScene(){
+        SceneManager.LoadScene("Scene", LoadSceneMode.Single);
+    }
+    public void Apturet(){
+        Application.Quit();
+    }
+    public void uzScene2(){
+        SceneManager.LoadScene("Scene2", LoadSceneMode.Single);
+    }
+	public void uzSakums(){
+        SceneManager.LoadScene("Sakums", LoadSceneMode.Single);
+    }
+}
